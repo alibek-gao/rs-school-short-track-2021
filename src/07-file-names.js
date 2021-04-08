d051349837b4ff863a9ca13f88a75e996591b8d3
alibek-gao <alibek.gao@gmail.com>
2021-04-08 11:38:57 +0300

    07

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -1,3 +1,11 @@
+function generate(name, existingNames, iteration = 0, original = '') {
+  if (existingNames.includes(name)) {
+    const temp = original === '' ? name : original;
+    return generate(`${temp}(${iteration + 1})`, existingNames, iteration + 1, name);
+  }
+  return name;
+}
+
 /**
  * There's a list of file, since two files cannot have equal names,
  * the one which comes later will have a suffix (k),
@@ -13,8 +21,12 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const output = [];
+  names.forEach((name) => {
+    output.push(generate(name, output));
+  });
+  return output;
 }
 
 module.exports = renameFiles;
