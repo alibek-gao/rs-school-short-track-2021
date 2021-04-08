41503b25756e523a3b79b0a62ce1faa673f7d940
alibek.gao <a.kazhibekov@mail.ru>
2021-04-08 09:40:04 +0300

    5,6

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,9 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const min = Math.min(...(n.toString().split('')));
+  return parseInt(n.toString().replace(min, ''), 10);
 }
 
 module.exports = deleteDigit;
